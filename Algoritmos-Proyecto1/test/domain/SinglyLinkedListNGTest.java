2eba5389f2a63b342f961f5aeaa603488a2a7e26
juanp <juanp@DESKTOP-U5R5263>
2021-05-13 09:34:36 -0600

    Cambios-Juanpa

---
+++ Algoritmos-Proyecto1/test/domain/SinglyLinkedListNGTest.java
@@ -0,0 +1,20 @@
+package domain;
+
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author juanp
+ */
+public class SinglyLinkedListNGTest {
+    
+    public SinglyLinkedListNGTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {//Se va a hacer para hacer pruebas :3
+        
+        
+    }
+    
+}//end class
