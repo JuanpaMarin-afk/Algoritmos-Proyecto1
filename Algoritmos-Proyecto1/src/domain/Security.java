b662ff3ec5b2c35d0efdacef70bea4b1be4adccb
juanp <juanp@DESKTOP-U5R5263>
2021-05-14 16:22:17 -0600

    Cambios-Juanpa

---
+++ Algoritmos-Proyecto1/src/domain/Security.java
@@ -0,0 +1,63 @@
+package domain;
+
+/**
+ *
+ * @author juanp
+ */
+public class Security {
+
+    //Atributos
+    private String type;
+    private String user;
+    private String password;
+
+    //Constructor Sobrecargado
+    public Security(String type, String user, String password) {
+        this.type = type;
+        this.user = user;
+        this.password = password;
+    }
+
+    //Constructor Remove, Contains
+    public Security(String user) {
+        this.user = user;
+    }
+
+    //Constructor Copia
+    public Security(Security security) {
+        this.type = security.type;
+        this.user = security.user;
+        this.password = security.password;
+    }
+
+    //Set Y Get
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Security{" + "type=" + type + ", user=" + user + ", password=" + password + '}';
+    }
+    
+}//end class
